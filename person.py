c2f6016d8e8ca46ef755980813a84836832a9145
unknown <mbonigabalexis624@gmail.com>
2020-02-27 16:29:03 +0200

    my first project

---
+++ person.py
@@ -0,0 +1,33 @@
+# def my_function():
+#   print("Hello word")
+
+# my_function()
+# def my_function(fname, lname):
+#   print(fname + " " + lname)
+
+# my_function("mbonigaba", "alexis")
+
+
+# def  my_function(fname):
+#      print(fname + " alexis")
+
+#my_function( "mbonigaba")
+#my_function( "bienvenue")
+#my_function( "kadady")
+# def my_function( country ="norway"):
+#     print(" i am from"+country)
+# my_function("swerden")
+# my_function("india") 
+# my_function()
+# my_function("brazil")
+    
+# def my_function(food):
+#         for x in food:
+#             print(x)
+# fruits=["apple","banana","cherry"]
+# my_function(fruits)
+def number(x):
+    return 5*x
+print(number(3))
+print(number(5))
+print(number(9))
